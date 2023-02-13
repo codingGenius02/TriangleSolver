05e4e9b24dbd001490cbf068c7b14b414660a6a0
Ethan <Ethan@LAPTOP-N87886TG>
2023-02-12 20:44:22 -0500

    Add project files.

---
+++ TriangleTestClass/TriangleTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+using System.Security.Policy;
+
+namespace TriangleTestClass
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        Triangle objTesting = new Triangle();
+
+        //Only 1 test for valid equilateral triangle
+        [Test]
+        public void TestEquilateralTriangle_Input7andInput7andInput7_Equal()
+        {
+            //arrange
+            int firstSide = 7;
+            int secondSide = 7;
+            int thirdSide= 7;
+
+            string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+
+            //act
+            string actual = objTesting.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
